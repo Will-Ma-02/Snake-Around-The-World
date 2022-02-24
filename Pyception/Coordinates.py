612ee1fb87bd89c55db79dcd49409af497b36a4e
William Ma <80716324+Will-Ma-02@users.noreply.github.com>
2022-02-24 18:55:31 -0500

    Delete Pyception directory

--- Pyception/Coordinates.py
+++
@@ -1,6 +0,0 @@
-import Constants, math
-
-# All possible spawn coordinates for the random number generator
-
-VALID_COORDINATES = [i for i in range(int(math.sqrt(Constants.SCREEN_AREA)))
-if i % Constants.SEGMENT_SIZE == 0]
