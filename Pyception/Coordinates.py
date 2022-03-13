4a86ef3d0c91819a9668a0c6ede094ab037c8f4b
William Ma <80716324+Will-Ma-02@users.noreply.github.com>
2022-03-13 06:12:27 -0400

    Delete Pyception directory

--- Pyception/Coordinates.py
+++
@@ -1,7 +0,0 @@
-import Constants, math
-
-# All possible spawn coordinates for the random number generator
-
-# A list comprehension that contains the set of all valid coordinates where food can spawn
-VALID_COORDINATES = [i for i in range(int(math.sqrt(Constants.SCREEN_AREA)))
-if i % Constants.SEGMENT_SIZE == 0]
